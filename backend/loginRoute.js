137d5bb24fb5812c827a430123714c420f345056
Muhammad Hunain <141824020+M-Hunain5037@users.noreply.github.com>
2024-07-30 17:36:58 +0500

    Delete backend directory

--- backend/loginRoute.js
+++
@@ -1,9 +0,0 @@
-// loginRoute.js
-const express = require('express');
-const { loginUser } = require('./loginController');
-
-const router = express.Router();
-
-router.post('/login', loginUser);
-
-module.exports = router;
